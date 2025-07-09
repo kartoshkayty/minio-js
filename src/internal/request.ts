dfac9de96825433cdb38314579b8eed08972de2c
kartoshka <gudovdanil1234@gmail.com>
2025-07-10 02:16:05 +0700

    delete naher requestwithretry

--- src/internal/request.ts
+++ src/internal/request.ts
@@ -28,7 +28,7 @@ export async function request(
   })
 }
 
-const MAX_RETRIES = 2
+const MAX_RETRIES = 1
 const EXP_BACK_OFF_BASE_DELAY = 1000 // Base delay for exponential backoff
 const ADDITIONAL_DELAY_FACTOR = 1.0 // to avoid synchronized retries
 
